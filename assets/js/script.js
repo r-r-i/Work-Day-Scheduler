e982b6c3b7e3734fde792d38fe8a5f9654629226
brake <riaconoo@icloud.com>
2022-06-13 16:14:58 +1000

    buttons now clickable, set/get from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,61 @@
+// TODO
+    // (a) current date at top of page
+    // (b) page styled to mock-up
+    // (c) buttons are clickable and saves text to local storage
+    // (d) colors change when time is past,present,future
+
+
+
 // Global
 var today = document.getElementById("currentDay")
 var saveButton = document.querySelector(".saveBtn")
+var userEventSpan = document.querySelector(".textarea")
 
 
 today.textContent = moment().format('dddd, MMMM Do')
 
 
-saveButton.addEventListener("click", function(event){
-    event.preventDefault();
-    var textarea = document.querySelector("textarea").value;
-    localStorage.setItem("textarea", textarea)
-
-} )
 
 
-
-// function addToLocal(){
-//     localStorage.setItem("events")
-// }
-
-
-// function renderUserScore(){
-//     var userInitials = localStorage.getItem("initials");
-//     userInitialsSpan.textContent = userInitials;
-//     var userScore = localStorage.getItem("recentScore");
-//     userInitialsSpan.textContent = userInitials + " scored " + userScore + " points";
-// }
-
-
+// Function that adds eventListener on each button, saves to LS
 btns = document.getElementsByClassName("saveBtn");
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function (event) {
         event.preventDefault()
         var textarea = document.querySelector("textarea").value;
         localStorage.setItem("textarea", textarea)
-        //Add function here
+        
     });
+
 }
 
-// btns = document.getElementsByClassName("");
-// for (var i = 0; i < btns.length; i++) {
-//     btns[i].addEventListener("click", function (event) {
-//         event.preventDefault()
-//         var textarea = document.querySelector("textarea").value;
-//         localStorage.setItem("textarea", textarea)
-//         //Add function here
-//     });
-// }
+textarea = document.getElementsByClassName("textarea");
+for (var i = 0; i < textarea.length; i++) {
+    console.log("working")
+    textarea
+
+
+    btns[i].addEventListener("click", function (event) {
+        event.preventDefault()
+        var textarea = document.querySelector("textarea").value;
+        localStorage.setItem("textarea", textarea)
+        
+    });
+
+}
 
 
 
-
-
-
-
-
-
-
-// submitButton.addEventListener("click", function(event) {
-//     event.preventDefault();
-//     var initials = document.querySelector("#initials").value;
+function renderUserEvent(){
+    var userEvent = localStorage.getItem("textarea");
+    userEventSpan.textContent = userEvent;
     
-//     if (initials === "") {
-//       alert("Initials cannot be left blank.");
-//       return
-//     } 
-//       localStorage.setItem("initials", initials);
-//       hideEnd();
-//       showHigh();
-      
-//     renderUserScore();
-//     }
-//   );
+}
+
+
+renderUserEvent()
+
+
+
+
+
